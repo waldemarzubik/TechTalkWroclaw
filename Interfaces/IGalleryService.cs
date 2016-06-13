cb369449b24a7f7848ae3d804cdfcd423a017d23
Waldemar Zubik <wzubik@hotmail.com>
2016-06-13 12:49:01 +0200

    solution common interfaces

---
+++ Interfaces/IGalleryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using TechTalk.DataModels;
+
+namespace TechTalk.Interfaces
+{
+    public interface IGalleryService
+    {
+        Task<List<Picture>> LoadImagesAsync();
+    }
+}
---
+++ Interfaces/INavigation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TechTalk.Interfaces
+{
+    public interface INavigation
+    {
+        string CurrentPageKey
+        {
+            get;
+        }
+
+        void GoBack();
+
+        void NavigateTo(string pageKey);
+
+        void NavigateTo<T>(string pageKey, T parameter);
+    }
+}
