ab46f82ac55dbf28c6cfe98520e89a1094c2dad2
Artem Panfilov <coredeveloper@gmail.com>
2016-06-16 15:44:40 +0200

    picture fix

--- DataModels/Picture.cs
+++ DataModels/Picture.cs
@@ -19,18 +19,12 @@ namespace TechTalk.DataModels
         /// <value>The thumbnail URI.</value>
         public string ThumbnailUri { get; set; }
 
-<<<<<<< HEAD
+
 		/// <summary>
 		/// Optional
 		/// </summary>
 		/// <value>The thumbnail handler.</value>
 		public IntPtr ThumbnailHandle { get; set; }
-=======
-        /// <summary>
-        /// Optional
-        /// </summary>
-        /// <value>The thumbnail handler.</value>
-        public IntPtr ThumbnailHandler { get; set; }
->>>>>>> origin/master
+
     }
 }
