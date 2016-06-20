7419056216f8fd596036a5c2fe3c0f95009e5590
Waldemar Zubik <wzubik@hotmail.com>
2016-06-20 19:27:54 +0200

    Stage 1.

---
+++ Droid/Views/MainView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TechTalk.ViewModels;
+namespace TechTalk.Droid
+{
+    public class MainView : ActivityBase<IMainViewModel>
+    {
+        public MainView() : base(Resource.Layout.MainView, 0)
+        {
+        }
+    }
+}
+
---
+++ Droid/Views/PictureView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TechTalk.ViewModels;
+namespace TechTalk.Droid
+{
+    public class PictureView : ActivityBase<IPictureViewModel>
+    {
+        public PictureView() : base(Resource.Layout.PictureView, 0)
+        {
+        }
+    }
+}
+
